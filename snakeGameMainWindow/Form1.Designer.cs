f05c84b48df03c34605486eb8fb6ac97933bb562
wesoly97 <61495739+wesoly97@users.noreply.github.com>
2020-12-29 13:45:58 +0100

    Create Project: snakeGameMainWindow

---
+++ snakeGameMainWindow/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace snakeGameMainWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.snakeControl1 = new snakeControl.SnakeControl();
+            this.SuspendLayout();
+            // 
+            // snakeControl1
+            // 
+            this.snakeControl1.Location = new System.Drawing.Point(388, 22);
+            this.snakeControl1.Name = "snakeControl1";
+            this.snakeControl1.Size = new System.Drawing.Size(400, 400);
+            this.snakeControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.snakeControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private snakeControl.SnakeControl snakeControl1;
+    }
+}
+
--- snakeGameMainWindow/Form1.cs
+++ snakeGameMainWindow/Form1.cs
@@ -1,25 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace snakeControl
+namespace snakeGameMainWindow
 {
-    public partial class SnakeControl: UserControl
+    public partial class Form1 : Form
     {
-        public SnakeControl()
+        public Form1()
         {
             InitializeComponent();
-          
         }
-
-   
     }
-
-
 }
